b0cfadd7e714c2dc66a870ea96cec860a7994f51
vgnFran <francohoyos2112@gmail.com>
2023-02-04 01:35:04 -0300

    renegando

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
-// const swiper = new Swiper(".mySwiper", {
-//     spaceBetween: 30,
-//     centeredSlides: true,
-//     autoplay: {
-//       delay: 2500,
-//       disableOnInteraction: false,
-//     },
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
+const swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+
+
+// new Accordion('.accordion-container', {
+//     duration: 350,
+//     showMultiple: true,
 //   });
+
+// new Accordion('.accordion-container2', {
+// duration: 350,
+// showMultiple: true,
+// });
